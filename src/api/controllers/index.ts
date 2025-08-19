3263daa227205989ac19f2498b136af8537cf469
theHELDERscrolls <manuhelderruiz@gmail.com>
2025-08-19 17:28:39 +0200

    rename: user.controller changed to auth.controller

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -1,1 +1,1 @@
-export * from "./user.controller.js";
+export * from "./auth.controller.js";
--- src/api/routes/auth.route.ts
+++ src/api/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register } from "../controllers/user.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 
 export const authRouter = express.Router();
 
